2bb9130e7698eaaed1f455bc29fa4dd44d3c5d73
evopen <520dhh@gmail.com>
2020-01-22 19:02:52 +0800

    don't need catch2 anymore
    I'm using google test now

--- VulkanBackend/src/snippets/snippets_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "snippets.h"
-
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
-//TEST_CASE("what the fuck")
-//{
-//	REQUIRE(compile_shader("shaders") == "glslc.exe shaders -o shaders.spv");
-//}
